24c306d99a28d0b25c14485373b874c54b049df4
Chris Webb <kungpochicken@gmail.com>
2022-03-21 21:12:24 +1100

    adds fsjsd-demosite scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import DemoSiteLayoutContainer from "fsjsd-demosite";
+import CarouselContainer from './features/carousel/CarouselContainer';
 
 function App() {
   return (
-    <div className="App">
-      TODO
-    </div>
+    <DemoSiteLayoutContainer
+      renderHeader={
+        () => (<>
+          <b>
+            Front-End
+          </b>
+          {" "}
+          Design Patterns
+        </>)
+      }
+      renderNavigation={
+        () => (<div> {/* Filter control for nav */}
+          Nav
+        </div>)
+      }
+      renderContents={
+        () => (<>
+          <CarouselContainer />
+        </>)
+      } />
   );
 }
 
---
+++ src/features/carousel/CarouselContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from './Carousel'
+
+const CarouselContainer = () => {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  )
+}
+
+export default CarouselContainer
