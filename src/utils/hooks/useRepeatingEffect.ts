416823101a0a2acdc584f4cb17e46a5ccabf55ed
Chris Webb <kungpochicken@gmail.com>
2022-03-26 16:36:46 +1100

    exclude 3rd party file from coverage

--- src/utils/hooks/useRepeatingEffect.ts
+++ src/utils/hooks/useRepeatingEffect.ts
@@ -1,17 +1,18 @@
+/* istanbul ignore file */
 import { useEffect, useState } from "react";
 
 export const useRepeatingEffect = (effect: () => void, timeout: number) => {
   const [intervalId, setIntervalId] = useState<NodeJS.Timer>();
   useEffect(() => {
     (async () => {
-      console.log("get webvital", intervalId)
+      console.log("get webvital", intervalId);
       await effect();
       const interval = setTimeout(() => {
         setIntervalId(interval); //trigger re-run
       }, timeout);
     })();
     return () => {
-      intervalId && clearTimeout(intervalId)
-    }
-  }, [effect, intervalId, timeout])
+      intervalId && clearTimeout(intervalId);
+    };
+  }, [effect, intervalId, timeout]);
 };
