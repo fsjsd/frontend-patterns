8863419154420cf58b930d61e107a2d6f68ab2e0
Chris Webb <kungpochicken@gmail.com>
2022-03-26 15:29:29 +1100

    fixes carousel tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     react: {
-      version: 'latest',
+      version: '999.9.9',
     },
   },
   env: {
--- src/features/carousel/CarouselContainer.test.tsx
+++ src/features/carousel/CarouselContainer.test.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
 import CarouselContainer from './CarouselContainer'
 
-describe('CarouselContainer tests', () => {
-  test('renders Carousel', () => {
+describe('CarouselContainer', () => {
+  test('render matches snapshot', () => {
     const { container } = render(<CarouselContainer />)
+    expect(container).toMatchSnapshot()
+  })
+  test('window resize recalculates layout', async () => {
+    const { container } = render(<CarouselContainer />)
+    global.dispatchEvent(new Event('resize'))
     expect(container).toMatchSnapshot()
   })
   test('forward advances slide', async () => {
@@ -25,4 +30,27 @@ describe('CarouselContainer tests', () => {
     //const secondTab = screen.getByRole('tab', { name: 'slide 2' });
     //expect(secondTab.getAttribute("aria-selected")).toBe('true');
   })
-})
+  test('back advances slide', async () => {
+    render(<CarouselContainer />)
+    const slideContainer = screen.getByTestId('slidecontainer')
+    const slide = screen.getAllByRole('tabpanel')
+    expect(slide[0].offsetWidth).toBe(0)
+    const firstTab = screen.getByRole('tab', { name: 'slide 1' })
+    expect(firstTab.getAttribute('aria-selected')).toBe('true')
+    expect(slideContainer.scrollLeft).toBe(0)
+    const backButton = screen.getByRole('button', { name: 'back' })
+    fireEvent.click(backButton)
+    expect(slideContainer.scrollLeft).toBe(10)
+    fireEvent.click(backButton)
+    expect(slideContainer.scrollLeft).toBe(20)
+  });
+  test('slide indicator advances slide', async () => {
+    render(<CarouselContainer />)
+    screen.getByTestId('slidecontainer')
+    const slide = screen.getAllByRole('tabpanel')
+    expect(slide[0].offsetWidth).toBe(0)
+    const firstTab = screen.getByRole('tab', { name: 'slide 2' })
+    expect(firstTab.getAttribute('aria-selected')).toBe('false')
+    fireEvent.click(firstTab)
+  });
+});
