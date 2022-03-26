2e87934398af49d13a6461c19609a962e3c5cc6e
Chris Webb <kungpochicken@gmail.com>
2022-03-26 16:31:14 +1100

    exclude 3rd party file from coverage

--- src/utils/hooks/useEffectAsync.ts
+++ src/utils/hooks/useEffectAsync.ts
@@ -1,6 +1,7 @@
+/* istanbul ignore file */
 import { useEffect } from "react";
 
-export const useEffectAsync = (effect: () => void, deps: any[]) => {
+export const useEffectAsync = (effect: () => void, deps: unknown[]) => {
   useEffect(() => {
     (async () => {
       effect();
