d968f79e56729baab09f08afe26cdc92bfd8d574
Chris Webb <kungpochicken@gmail.com>
2022-03-27 22:29:06 +1100

    rollback infinite scroll

--- src/features/infinitescroll/InfiniteScrollContainer.test.tsx
+++ src/features/infinitescroll/InfiniteScrollContainer.test.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
-import { fireEvent, render, waitFor } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import InfiniteScrollContainer from './InfiniteScrollContainer'
 
 describe('InfiniteScrollContainer', () => {
   test('render matches snapshot', async () => {
-    const { container, getByRole, queryByRole, queryAllByRole } = render(<InfiniteScrollContainer />)
-    await waitFor(() => expect(queryByRole("list")).toBeInTheDocument());
-    const scrollWrapper = getByRole("list");
-    expect(container).toBeDefined();
+    const { container, queryByRole } = render(<InfiniteScrollContainer />)
+    await waitFor(() => expect(queryByRole("main")).toBeInTheDocument());
     expect(container).toMatchSnapshot();
-    const items = await queryAllByRole("listitem");
-    expect(items.length).toBe(5)
-
-    expect(scrollWrapper.scrollTop).toBe(0);
-    fireEvent.scroll(scrollWrapper, { target: { scrollTop: -10000 } });
-    expect(scrollWrapper?.scrollTop).toBe(-10000);
-
-    // await waitFor(() => expect(queryByRole("button", { name: "Load more" })?.getAttribute("disabled")).toBe("false"));
   })
 })
--- src/features/infinitescroll/InfiniteScrollContainer.tsx
+++ src/features/infinitescroll/InfiniteScrollContainer.tsx
@@ -1,38 +1,9 @@
-import React, { Suspense, useCallback } from 'react'
+import React, { Suspense } from 'react'
 import { ContentWrapper } from '../../ux/ContentWrapper'
 import Loading from '../../ux/Loading'
 const InfiniteScroll = React.lazy(() => import('./InfiniteScroll'))
-//const requirementsMarkDownPromise = import('./requirements.md')
-//import mdFile from "./requirements.md";
-//console.log(requirementsMarkDown)
-interface ScrollItemData {
-  id: number
-}
-
-let idSeed = 1;
-
-const generateTestData = () => new Array(5).fill(null).map(() => ({ id: idSeed++ }));
-
-const firstPageData = generateTestData();
-
-const mapItemComponent = (data: unknown) => <div role="listitem">{(data as ScrollItemData).id}</div>;
 
 const InfiniteScrollContainer = () => {
-
-  const initialData: ScrollItemData[] = firstPageData;
-
-  const getMore = useCallback(() => {
-    return new Promise<{ data: ScrollItemData[], hasMore: boolean }>((resolve) => {
-      const data: ScrollItemData[] = generateTestData();
-      setTimeout(() => {
-        console.log('InfiniteScrollContainer.loadMore END', data);
-        resolve({
-          data,
-          hasMore: data[0].id < 10 // simple hack to stop data loading after ~50 entries
-        });
-      }, 500);
-    });
-  }, []);
 
   return (
     <ContentWrapper
@@ -42,11 +13,7 @@ const InfiniteScrollContainer = () => {
     >
       <Suspense fallback={<Loading />}>
         <div style={{ width: 300, height: 500, overflowY: "scroll" }}>
-          <InfiniteScroll
-            data={initialData}
-            renderData={mapItemComponent}
-            onLoadMore={getMore}
-          />
+          <InfiniteScroll />
         </div>
       </Suspense>
     </ContentWrapper>
