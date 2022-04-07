b0b583618eece65763cf9d165608a358ac7d0733
Chris Webb <kungpochicken@gmail.com>
2022-04-07 20:29:54 +1000

    updates typeahead readme

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,3 +4,11 @@
 declare module '*.md'
 
 declare module "fsjsd-demosite"
+
+// see:
+// https://stackoverflow.com/questions/61667608/how-do-you-correctly-use-react-lazy-in-typescript-to-import-a-react-component
+declare namespace React {
+  function lazy<T extends ComponentType<any>>(
+    factory: () => Promise<{ default: T }>,
+  ): T;
+}
