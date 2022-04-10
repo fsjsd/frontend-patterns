b931ed80a3b750d2a931ae99a1d4835b77f7f5dc
Chris Webb <kungpochicken@gmail.com>
2022-04-10 22:43:54 +1000

    removes incomplete scroll work from main branch

--- src/features/infinitescroll/InfiniteScrollContainer.tsx
+++ src/features/infinitescroll/InfiniteScrollContainer.tsx
@@ -1,14 +1,7 @@
 import React, { Suspense } from 'react'
 import { ContentWrapper } from '../../ux/ContentWrapper'
 import Loading from '../../ux/Loading'
-import { FeedItem, getItem } from './utils/data';
-import { db } from "./utils/db";
-
-// import RecyclingVirtualList from './recyclingvirtuallist/RecyclingVirtualList'
-const RecyclingVirtualList = React.lazy(() => import('./recyclingvirtuallist/RecyclingVirtualList'))
-//const InfiniteScroll = React.lazy(() => import('./InfiniteScroll'))
-
-const DB = db(1000, 1000, getItem);
+const InfiniteScroll = React.lazy(() => import('./InfiniteScroll'))
 
 const InfiniteScrollContainer = () => {
 
@@ -19,12 +12,8 @@ const InfiniteScrollContainer = () => {
       markDownPromise={import('./requirements.md')}
     >
       <Suspense fallback={<Loading />}>
-        <div role="main" style={{ width: 300, height: 500, overflowY: "scroll" }}>
-          <RecyclingVirtualList
-            pageSize={10}
-            load={(start, limit) => DB.load(start, limit).then((cursor) => cursor.chunk) as Promise<FeedItem[]>}
-            itemFullComponent={(item: FeedItem) => <div>{item.name}</div>}
-          />
+        <div role="main">
+          <InfiniteScroll />
         </div>
       </Suspense>
     </ContentWrapper>
