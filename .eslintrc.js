da6a6e86df8ec119fcaa55c932d2ed40a49b3f5b
Chris Webb <kungpochicken@gmail.com>
2022-04-11 17:21:19 +1000

    adds jsx a11y lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     "plugin:cypress/recommended"
