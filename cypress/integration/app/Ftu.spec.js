28c3eb413ab8e3989fd83649a220083ac358e151
Chris Webb <kungpochicken@gmail.com>
2022-04-11 20:01:43 +1000

    add cypress config to see violations in ci

--- cypress/integration/app/Ftu.spec.js
+++ cypress/integration/app/Ftu.spec.js
@@ -4,9 +4,28 @@ beforeEach(() => {
   cy.injectAxe();
 })
 
+// Define at the top of the spec file or just import it
+function terminalLog(violations) {
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${violations.length === 1 ? '' : 's'
+    } ${violations.length === 1 ? 'was' : 'were'} detected`
+  )
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length
+    })
+  )
+
+  cy.task('table', violationData)
+}
 
 describe('FTU', () => {
   it('Should not violate a11y rules', () => {
-    cy.checkA11y();
+    cy.checkA11y(null, null, terminalLog)
   })
 })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,20 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+module.exports = (on) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  on('task', {
+    log(message) {
+      console.log(message)
+
+      return null
+    },
+    table(message) {
+      console.table(message)
+
+      return null
+    }
+  })
 }
