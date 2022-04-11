006f0bc1aa861ff8a721b7804d68fdd51c6db8e3
Chris Webb <kungpochicken@gmail.com>
2022-04-11 20:58:43 +1000

    updates post main branch merge

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -8,7 +8,10 @@ declare module "fsjsd-demosite"
 // see:
 // https://stackoverflow.com/questions/61667608/how-do-you-correctly-use-react-lazy-in-typescript-to-import-a-react-component
 declare namespace React {
-  function lazy<T extends ComponentType<unknown>>(
+  // this has to be ComponentType<any> or type inference doesn't work on 
+  // lazy loaded components
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function lazy<T extends ComponentType<any>>(
     factory: () => Promise<{ default: T }>,
   ): T;
 }
