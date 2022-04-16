f2efed3456e19802c50de942d19f6bef56821e05
Chris Webb <kungpochicken@gmail.com>
2022-04-16 18:05:08 +1000

    Updates

---
+++ static/js/747.e905b915.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend_patterns=self.webpackChunkfrontend_patterns||[]).push([[747],{8747:function(e,t,n){e.exports=n.p+"static/media/requirements.44ed5f6d8fb8ce66e5c3.md"}}]);
--- static/media/requirements.1540416a828cdfd466e8.md
+++
@@ -1,5 +0,0 @@
-# Requirements
-
-- Replacement for native img component
-- Renders placeholder component while waiting for image src to load
-- Off screen images not loaded
